4c358be89845008481f79f082b6e73e931eaeced
kicaj <kicaj>
2019-05-31 14:03:45 +0200

    small update

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -15,10 +15,6 @@ export let appRoutes: Routes = [
     loadChildren: './tree-shakeable-providers/tree-shakeable-providers.module#TreeShakeableProvidersModule'
   },
   {
-    path: 'adapt-tree-shakeable-providers',
-    loadChildren: './adapt-tree-shakeable-providers/adapt-tree-shakeable-providers.module#AdaptTreeShakeableProvidersModule'
-  },
-  {
     path: 'tab-col-resize',
     component: TableColResizeComponent
   },
